a53041b1f922b2be46aff4fcd0a6d89f1a4d30fd
Josh Merlin <jmerlin.suf@gmail.com>
2022-12-16 22:55:48 +0800

    remove extra files

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,2 @@
-export { default as useGame } from './useGame';
-export { default as useTask } from './useTask';
 export { default as useTimer } from './useTimer';
 export { default as useTheme } from './useTheme';
